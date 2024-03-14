1cb7dba6b4452a052884ccccd0830a29146f007e
hiephtdev <work.hiepht@gmail.com>
2024-03-14 23:01:10 +0700

    perform code

--- backpack_client.js
+++ backpack_client.js
@@ -1,17 +1,13 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BackpackClient = void 0;
-const got_1 = __importDefault(require("got"));
-const crypto_1 = __importDefault(require("crypto"));
-const qs_1 = __importDefault(require("qs"));
-const ws_1 = __importDefault(require("ws"));
+import got from "got";
+import crypto from "crypto";
+import qs from "qs";
+import WebSocket from "ws";
+
 const BACKOFF_EXPONENT = 1.5;
 const DEFAULT_TIMEOUT_MS = 5000;
-// const BASE_URL = "https://api.backpack.exchange/";
 const BASE_URL = "https://api.backpack.exchange/";
+
 const instructions = {
     public: new Map([
         ["assets", { url: `${BASE_URL}api/v1/assets`, method: "GET" }],
@@ -23,82 +19,54 @@ const instructions = {
         ["ping", { url: `${BASE_URL}api/v1/ping`, method: "GET" }],
         ["time", { url: `${BASE_URL}api/v1/time`, method: "GET" }],
         ["trades", { url: `${BASE_URL}api/v1/trades`, method: "GET" }],
-        [
-            "tradesHistory",
-            { url: `${BASE_URL}api/v1/trades/history`, method: "GET" },
-        ],
+        ["tradesHistory", { url: `${BASE_URL}api/v1/trades/history`, method: "GET" }],
     ]),
     private: new Map([
         ["balanceQuery", { url: `${BASE_URL}api/v1/capital`, method: "GET" }],
-        [
-            "depositAddressQuery",
-            { url: `${BASE_URL}wapi/v1/capital/deposit/address`, method: "GET" },
-        ],
-        [
-            "depositQueryAll",
-            { url: `${BASE_URL}wapi/v1/capital/deposits`, method: "GET" },
-        ],
-        [
-            "fillHistoryQueryAll",
-            { url: `${BASE_URL}wapi/v1/history/fills`, method: "GET" },
-        ],
+        ["depositAddressQuery", { url: `${BASE_URL}wapi/v1/capital/deposit/address`, method: "GET" }],
+        ["depositQueryAll", { url: `${BASE_URL}wapi/v1/capital/deposits`, method: "GET" }],
+        ["fillHistoryQueryAll", { url: `${BASE_URL}wapi/v1/history/fills`, method: "GET" }],
         ["orderCancel", { url: `${BASE_URL}api/v1/order`, method: "DELETE" }],
         ["orderCancelAll", { url: `${BASE_URL}api/v1/orders`, method: "DELETE" }],
         ["orderExecute", { url: `${BASE_URL}api/v1/order`, method: "POST" }],
-        [
-            "orderHistoryQueryAll",
-            { url: `${BASE_URL}wapi/v1/history/orders`, method: "GET" },
-        ],
+        ["orderHistoryQueryAll", { url: `${BASE_URL}wapi/v1/history/orders`, method: "GET" }],
         ["orderQuery", { url: `${BASE_URL}api/v1/order`, method: "GET" }],
         ["orderQueryAll", { url: `${BASE_URL}api/v1/orders`, method: "GET" }],
-        [
-            "withdraw",
-            { url: `${BASE_URL}wapi/v1/capital/withdrawals`, method: "POST" },
-        ],
-        [
-            "withdrawalQueryAll",
-            { url: `${BASE_URL}wapi/v1/capital/withdrawals`, method: "GET" },
-        ],
+        ["withdraw", { url: `${BASE_URL}wapi/v1/capital/withdrawals`, method: "POST" }],
+        ["withdrawalQueryAll", { url: `${BASE_URL}wapi/v1/capital/withdrawals`, method: "GET" }],
     ]),
 };
-// https://stackoverflow.com/questions/71916954/crypto-sign-function-to-sign-a-message-with-given-private-key
+
 const toPkcs8der = (rawB64) => {
     var rawPrivate = Buffer.from(rawB64, "base64").subarray(0, 32);
     var prefixPrivateEd25519 = Buffer.from("302e020100300506032b657004220420", "hex");
     var der = Buffer.concat([prefixPrivateEd25519, rawPrivate]);
-    return crypto_1.default.createPrivateKey({ key: der, format: "der", type: "pkcs8" });
+    return crypto.createPrivateKey({ key: der, format: "der", type: "pkcs8" });
 };
-// https://stackoverflow.com/questions/68612396/sign-and-verify-jws-json-web-signature-with-ed25519-keypair
+
 const toSpki = (rawB64) => {
     var rawPublic = Buffer.from(rawB64, "base64");
     var prefixPublicEd25519 = Buffer.from("302a300506032b6570032100", "hex");
     var der = Buffer.concat([prefixPublicEd25519, rawPublic]);
-    return crypto_1.default.createPublicKey({ key: der, format: "der", type: "spki" });
+    return crypto.createPublicKey({ key: der, format: "der", type: "spki" });
 };
-/**
- * https://docs.backpack.exchange/#section/Authentication/Signing-requests
- * @param  {Object}        request params as an object
- * @param  {UInt8Array}    privateKey
- * @param  {number}        timestamp Unix time in ms that the request was sent
- * @param  {string}        instruction
- * @param  {number}        window Time window in milliseconds that the request is valid for
- * @return {string}        base64 encoded signature to include on request
- */
+
 const getMessageSignature = (request, privateKey, timestamp, instruction, window) => {
     function alphabeticalSort(a, b) {
         return a.localeCompare(b);
     }
-    const message = qs_1.default.stringify(request, { sort: alphabeticalSort });
+    const message = qs.stringify(request, { sort: alphabeticalSort });
     const headerInfo = { timestamp, window: window ?? DEFAULT_TIMEOUT_MS };
-    const headerMessage = qs_1.default.stringify(headerInfo);
+    const headerMessage = qs.stringify(headerInfo);
     const messageToSign = "instruction=" +
         instruction +
         "&" +
         (message ? message + "&" : "") +
         headerMessage;
-    const signature = crypto_1.default.sign(null, Buffer.from(messageToSign), toPkcs8der(privateKey));
+    const signature = crypto.sign(null, Buffer.from(messageToSign), toPkcs8der(privateKey));
     return signature.toString("base64");
 };
+
 const rawRequest = async (instruction, headers, data) => {
     const { url, method } = instructions.private.has(instruction)
         ? instructions.private.get(instruction)
@@ -113,7 +81,7 @@ const rawRequest = async (instruction, headers, data) => {
     if (method == "GET") {
         Object.assign(options, { method });
         fullUrl =
-            url + (Object.keys(data).length > 0 ? "?" + qs_1.default.stringify(data) : "");
+            url + (Object.keys(data).length > 0 ? "?" + qs.stringify(data) : "");
     }
     else if (method == "POST" || method == "DELETE") {
         Object.assign(options, {
@@ -121,7 +89,7 @@ const rawRequest = async (instruction, headers, data) => {
             body: JSON.stringify(data),
         });
     }
-    const response = await (0, got_1.default)(fullUrl, options);
+    const response = await got(fullUrl, options);
     const contentType = response.headers["content-type"];
     if (contentType?.includes("application/json")) {
         const parsed = JSON.parse(response.body, function (_key, value) {
@@ -151,17 +119,11 @@ const rawRequest = async (instruction, headers, data) => {
         return response;
     }
 };
-/**
- * BackpackClient connects to the Backpack API
- * @param {string}        privateKey base64 encoded
- * @param {string}        publicKey  base64 encoded
- */
+
 class BackpackClient {
     constructor(privateKey, publicKey) {
         this.config = { privateKey, publicKey };
-        // Verify that the keys are a correct pair before sending any requests. Ran
-        // into errors before with that which were not obvious.
-        const pubkeyFromPrivateKey = crypto_1.default
+        const pubkeyFromPrivateKey = crypto
             .createPublicKey(toPkcs8der(privateKey))
             .export({ format: "der", type: "spki" })
             .toString("base64");
@@ -169,16 +131,10 @@ class BackpackClient {
             .export({ format: "der", type: "spki" })
             .toString("base64");
         if (pubkeyFromPrivateKey != pubkey) {
-            throw new Error("错误的秘钥对，请检查私钥公钥是否匹配");
+            throw new Error("Incorrect key pair, please check if the private and public keys match");
         }
     }
-    /**
-     * This method makes a public or private API request.
-     * @param  {String}   method
-     * @param  {Object}   params   Arguments to pass to the api call
-     * @param  {Number}   retrysLeft
-     * @return {Object}   The response object
-     */
+
     async api(method, params, retrysLeft = 10) {
         try {
             if (instructions.public.has(method)) {
@@ -206,22 +162,12 @@ class BackpackClient {
         }
         throw new Error(method + " is not a valid API method.");
     }
-    /**
-     * This method makes a public API request.
-     * @param  {String}   instruction   The API method (public or private)
-     * @param  {Object}   params        Arguments to pass to the api call
-     * @return {Object}                 The response object
-     */
+
     async publicMethod(instruction, params = {}) {
         const response = await rawRequest(instruction, {}, params);
         return response;
     }
-    /**
-     * This method makes a private API request.
-     * @param  {String}   instruction The API method (public or private)
-     * @param  {Object}   params      Arguments to pass to the api call
-     * @return {Object}               The response object
-     */
+
     async privateMethod(instruction, params = {}) {
         const timestamp = Date.now();
         const signature = getMessageSignature(params, this.config.privateKey, timestamp, instruction);
@@ -234,144 +180,97 @@ class BackpackClient {
         const response = await rawRequest(instruction, headers, params);
         return response;
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Capital/operation/get_balances
-     */
+
     async Balance() {
         return this.api("balanceQuery");
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Capital/operation/get_deposits
-     */
+
     async Deposits(params) {
         return this.api("depositQueryAll", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Capital/operation/get_deposit_address
-     */
+
     async DepositAddress(params) {
         return this.api("depositAddressQuery", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Capital/operation/get_withdrawals
-     */
+
     async Withdrawals(params) {
         return this.api("withdrawalQueryAll", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Capital/operation/request_withdrawal
-     */
+
     async Withdraw(params) {
         this.api("withdraw", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/History/operation/get_order_history
-     */
+
     async OrderHistory(params) {
         return this.api("orderHistoryQueryAll", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/History/operation/get_fills
-     */
+
     async FillHistory(params) {
         return this.api("fillHistoryQueryAll", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Markets/operation/get_assets
-     */
+
     async Assets() {
         return this.api("assets");
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Markets/operation/get_markets
-     */
+
     async Markets() {
         return this.api("markets");
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Markets/operation/get_ticker
-     */
+
     async Ticker(params) {
         return this.api("ticker", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Markets/operation/get_depth
-     */
+
     async Depth(params) {
         return this.api("depth", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Markets/operation/get_klines
-     */
+
     async KLines(params) {
         return this.api("klines", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Order/operation/get_order
-     */
+
     async GetOrder(params) {
         return this.api("orderQuery", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Order/operation/execute_order
-     */
+
     async ExecuteOrder(params) {
         return this.api("orderExecute", params, 3);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Order/operation/cancel_order
-     */
+
     async CancelOrder(params) {
         return this.api("orderCancel", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Order/operation/get_open_orders
-     */
+
     async GetOpenOrders(params) {
         return this.api("orderQueryAll", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Order/operation/cancel_open_orders
-     */
+
     async CancelOpenOrders(params) {
         return this.api("orderCancelAll", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/System/operation/get_status
-     */
+
     async Status() {
         return this.api("status");
     }
-    /**
-     * https://docs.backpack.exchange/#tag/System/operation/ping
-     */
+
     async Ping() {
         return this.api("ping");
     }
-    /**
-     * https://docs.backpack.exchange/#tag/System/operation/get_time
-     */
+
     async Time() {
         return this.api("time");
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Trades/operation/get_recent_trades
-     */
+
     async RecentTrades(params) {
         return this.api("trades", params);
     }
-    /**
-     * https://docs.backpack.exchange/#tag/Trades/operation/get_historical_trades
-     */
+
     async HistoricalTrades(params) {
         return this.api("tradesHistory", params);
     }
-    /**
-      * https://docs.backpack.exchange/#tag/Streams/Private
-      * @return {Object} Websocket     Websocket connecting to order update stream
-      */
+
     subscribeOrderUpdate() {
-        const privateStream = new ws_1.default('wss://ws.backpack.exchange');
+        const privateStream = new WebSocket('wss://ws.backpack.exchange');
         const timestamp = Date.now();
         const window = 5000;
         const signature = getMessageSignature({}, this.config.privateKey, timestamp, "subscribe", window);
@@ -381,13 +280,14 @@ class BackpackClient {
             "signature": [this.config.publicKey, signature, timestamp.toString(), window.toString()]
         };
         privateStream.onopen = (_) => {
-            console.log('Connected to BPX Websocket');
+            console.log('Connected to BPX WebSocket');
             privateStream.send(JSON.stringify(subscriptionData));
         };
         privateStream.onerror = (error) => {
-            console.log(`Websocket Error ${error}`);
+            console.log(`WebSocket Error ${error}`);
         };
         return privateStream;
     }
 }
-exports.BackpackClient = BackpackClient;
+
+export { BackpackClient };
--- index.js
+++ index.js
@@ -1,189 +1,196 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const backpack_client_1 = require("./backpack_client");
 
-/// EDIT HERE ///
-const API_KEY = "SxxxxxxTc="
-const API_SECRET = "Bxxxxxxxg="
-const MAX_VOLUME = 111000
-const SWAP_VOLUME = [100,220]
+import { BackpackClient } from "./backpack_client.js";
+
+/// EDIT ///
+const API_KEY = "0hnpbVxxxxxxxxxxxOg=";
+const API_SECRET = "sG2xxxxxxxxxxxxxxxdQoc=";
+const MAX_VOLUME = 111000;
+const SWAP_VOLUME = [100, 220];
 /////////////
 
 const wait = async (ms) => {
     return new Promise(resolve => {
         setTimeout(resolve, ms);
     });
-}
+};
 
-function getNowFormatDate() {
-    var date = new Date();
-    var seperator1 = "-";
-    var seperator2 = ":";
-    var month = date.getMonth() + 1;
-    var strDate = date.getDate();
-    var strHour = date.getHours();
-    var strMinute = date.getMinutes();
-    var strSecond = date.getSeconds();
-    if (month >= 1 && month <= 9) {
-        month = "0" + month;
-    }
-    if (strDate >= 0 && strDate <= 9) {
-        strDate = "0" + strDate;
-    }
-    if (strHour >= 0 && strHour <= 9) {
-        strHour = "0" + strHour;
-    }
-    if (strMinute >= 0 && strMinute <= 9) {
-        strMinute = "0" + strMinute;
-    }
-    if (strSecond >= 0 && strSecond <= 9) {
-        strSecond = "0" + strSecond;
-    }
-    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
-        + " " + strHour + seperator2 + strMinute
-        + seperator2 + strSecond;
+const getNowFormatDate = () => {
+    const date = new Date();
+    const seperator1 = "-";
+    const seperator2 = ":";
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const strDate = date.getDate().toString().padStart(2, "0");
+    const strHour = date.getHours().toString().padStart(2, "0");
+    const strMinute = date.getMinutes().toString().padStart(2, "0");
+    const strSecond = date.getSeconds().toString().padStart(2, "0");
+    const currentdate = `${date.getFullYear()}${seperator1}${month}${seperator1}${strDate} ${strHour}${seperator2}${strMinute}${seperator2}${strSecond}`;
     return currentdate;
-}
+};
 
-let successbuy = 0;
-let sellbuy = 0;
+let successBuy = 0;
+let sellBuy = 0;
 let cancelJob = false;
 let buyFlag = true;
 let totalVolume = 0;
+
 const init = async (client) => {
     try {
         if (cancelJob) {
             console.log(getNowFormatDate(), "Job canceled");
             return;
         }
-        console.log("\n============================")
-        console.log(`Total Buy: ${successbuy} | Total Sell: ${sellbuy} | Total Volume: ${totalVolume} USDC`);
-        console.log("============================\n")
+
+        console.log("\n============================");
+        console.log(`Total Buy: ${successBuy} | Total Sell: ${sellBuy} | Total Volume: ${totalVolume} USDC`);
+        console.log("============================\n");
 
         console.log(getNowFormatDate(), "Waiting 10 seconds...");
         await wait(10000);
 
-        let userbalance = await client.Balance();
-        let usdcTradeAvailable = randomFloat(SWAP_VOLUME[0], SWAP_VOLUME[1]);
-        if (userbalance.USDC.available < usdcTradeAvailable) {
-            console.log("Not enough USDC balance!")
+        const userBalance = await client.Balance();
+        const usdcTradeAvailable = randomFloat(SWAP_VOLUME[0], SWAP_VOLUME[1]);
+
+        if (userBalance.USDC.available < usdcTradeAvailable) {
+            console.log("Not enough USDC balance!");
             cancelJob = true;
             return;
         }
+
         if (totalVolume > MAX_VOLUME) {
-            console.log("Job done!")
+            console.log("Job done!");
             cancelJob = true;
             return;
         }
+
         if (buyFlag) {
-            await buyfun(client, usdcTradeAvailable);
+            await buyFun(client, usdcTradeAvailable);
         } else {
-            await sellfun(client);
+            await sellFun(client);
             return;
         }
-    } catch (e) {
-        console.log(getNowFormatDate(), `Try again... (${e.message})`);
-        console.log("=======================")
+    } catch (error) {
+        console.log(getNowFormatDate(), `Try again... (${error.message})`);
+        console.log("=======================");
 
         await wait(3000);
         init(client);
-
     }
-}
+};
 
 const random = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 const randomFloat = (min, max) => {
     return Math.random() * (max - min) + min;
-}
+};
 
-const sellfun = async (client) => {
-    let GetOpenOrders = await client.GetOpenOrders({ symbol: "SOL_USDC" });
-    if (GetOpenOrders.length > 0) {
-        let CancelOpenOrders = await client.CancelOpenOrders({ symbol: "SOL_USDC" });
-        console.log(getNowFormatDate(), "All pending orders canceled");
-    }
+const sellFun = async (client) => {
+    try {
+        const openOrders = await client.GetOpenOrders({ symbol: "SOL_USDC" });
 
-    let userbalance = await client.Balance();
-    console.log(getNowFormatDate(), `My Account Infos: ${userbalance.SOL.available} $SOL | ${userbalance.USDC.available} $USDC`,);
-
-    let { lastPrice: lastPriceask } = await client.Ticker({ symbol: "SOL_USDC" });
-    console.log(getNowFormatDate(), "Price sol_usdc:", lastPriceask);
-    let quantitys = (userbalance.SOL.available - 0.02).toFixed(2).toString();
-    if (Number.parseFloat(quantitys) < 0.01 || Number.parseFloat(userbalance.SOL.available) < 0.02) {
-        buyFlag = true;
-        throw new Error("Sell not enough SOL balance");
-    }
-    console.log(getNowFormatDate(), `Trade... ${quantitys} $SOL to ${(lastPriceask * quantitys).toFixed(2)} $USDC`);
-    let orderResultAsk = await client.ExecuteOrder({
-        orderType: "Limit",
-        price: (lastPriceask - 0.1).toFixed(2).toString(),
-        quantity: quantitys,
-        side: "Ask",
-        symbol: "SOL_USDC"
-    })
-    if (orderResultAsk?.status == "Filled" && orderResultAsk?.side == "Ask") {
-        sellbuy += 1;
-        console.log(getNowFormatDate(), "Sold successfully:", `Order number:${orderResultAsk.id}`);
-        totalVolume += parseFloat(lastPriceask * quantitys);
-        buyFlag = true;
-        init(client);
-    } else {
-        if (orderResultAsk?.status == 'Expired') {
-            throw new Error("Sell Order Expired");
-        } else {
-
-            throw new Error(orderResultAsk?.status);
+        if (openOrders.length > 0) {
+            await client.CancelOpenOrders({ symbol: "SOL_USDC" });
+            console.log(getNowFormatDate(), "All pending orders canceled");
         }
-    }
-}
 
-const buyfun = async (client, usdc) => {
-    let GetOpenOrders = await client.GetOpenOrders({ symbol: "SOL_USDC" });
-    if (GetOpenOrders.length > 0) {
-        let CancelOpenOrders = await client.CancelOpenOrders({ symbol: "SOL_USDC" });
-        console.log(getNowFormatDate(), "All pending orders canceled");
-    }
-    let userbalance = await client.Balance();
-    let balanceSol = 0;
-    if (userbalance.SOL) {
-        balanceSol = userbalance.SOL.available
-    }
-    console.log(getNowFormatDate(), `My Account Infos: ${balanceSol} $SOL | ${userbalance.USDC.available} $USDC`,);
-    let { lastPrice } = await client.Ticker({ symbol: "SOL_USDC" });
-    console.log(getNowFormatDate(), "Price of sol_usdc:", lastPrice);
-    let quantitys = (usdc / lastPrice).toFixed(2).toString();
-    console.log(getNowFormatDate(), `Trade ... ${(usdc).toFixed(2).toString()} $USDC to ${quantitys} $SOL`);
-    let orderResultBid = await client.ExecuteOrder({
-        orderType: "Limit",
-        price: (lastPrice + 0.1).toFixed(2).toString(),
-        quantity: quantitys,
-        side: "Bid",
-        symbol: "SOL_USDC"
-    })
-    buyFlag = false;
-    if (orderResultBid?.status == "Filled" && orderResultBid?.side == "Bid") {
-        successbuy += 1;
-        console.log(getNowFormatDate(), "Bought successfully:", `Order number: ${orderResultBid.id}`);
-        console.log(getNowFormatDate(), "Begin wait", `Next order number`);
-        totalVolume += usdc;
-        await wait(random(5, 10) * 1000);
-        console.log(getNowFormatDate(), "End wait", `Next order ...`);
-        init(client);
-    } else {
-        if (orderResultBid?.status == 'Expired') {
-            throw new Error("Buy Order Expired");
-        } else {
-            throw new Error(orderResultBid?.status);
+        const userBalance = await client.Balance();
+        console.log(getNowFormatDate(), `My Account Infos: ${userBalance.SOL.available} $SOL | ${userBalance.USDC.available} $USDC`);
+
+        const { lastPrice: lastPriceAsk } = await client.Ticker({ symbol: "SOL_USDC" });
+        console.log(getNowFormatDate(), "Price sol_usdc:", lastPriceAsk);
+
+        let quantitys = (userBalance.SOL.available - 0.02).toFixed(2).toString();
+        
+        if (Number.parseFloat(quantitys) < 0.01 || Number.parseFloat(userBalance.SOL.available) < 0.02) {
+            buyFlag = true;
+            throw new Error("Sell not enough SOL balance");
         }
+
+        console.log(getNowFormatDate(), `Trade... ${quantitys} $SOL to ${(lastPriceAsk * quantitys).toFixed(2)} $USDC`);
+        
+        const orderResultAsk = await client.ExecuteOrder({
+            orderType: "Limit",
+            price: (lastPriceAsk - 0.1).toFixed(2).toString(),
+            quantity: quantitys,
+            side: "Ask",
+            symbol: "SOL_USDC"
+        });
+
+        if (orderResultAsk?.status === "Filled" && orderResultAsk?.side === "Ask") {
+            sellBuy += 1;
+            console.log(getNowFormatDate(), "Sold successfully:", `Order number:${orderResultAsk.id}`);
+            totalVolume += parseFloat(lastPriceAsk * quantitys);
+            buyFlag = true;
+            init(client);
+        } else {
+            if (orderResultAsk?.status === 'Expired') {
+                throw new Error("Sell Order Expired");
+            } else {
+                throw new Error(orderResultAsk?.status);
+            }
+        }
+    } catch (error) {
+        console.log(getNowFormatDate(), `Error in selling: ${error.message}`);
+        console.log("=======================");
     }
-}
+};
+
+const buyFun = async (client, usdc) => {
+    try {
+        const openOrders = await client.GetOpenOrders({ symbol: "SOL_USDC" });
+
+        if (openOrders.length > 0) {
+            await client.CancelOpenOrders({ symbol: "SOL_USDC" });
+            console.log(getNowFormatDate(), "All pending orders canceled");
+        }
+
+        const userBalance = await client.Balance();
+        const balanceSol = userBalance.SOL ? userBalance.SOL.available : 0;
+
+        console.log(getNowFormatDate(), `My Account Infos: ${balanceSol} $SOL | ${userBalance.USDC.available} $USDC`,);
+
+        const { lastPrice } = await client.Ticker({ symbol: "SOL_USDC" });
+        console.log(getNowFormatDate(), "Price of sol_usdc:", lastPrice);
+
+        const quantitys = (usdc / lastPrice).toFixed(2).toString();
+        console.log(getNowFormatDate(), `Trade ... ${(usdc).toFixed(2).toString()} $USDC to ${quantitys} $SOL`);
+
+        const orderResultBid = await client.ExecuteOrder({
+            orderType: "Limit",
+            price: (lastPrice + 0.1).toFixed(2).toString(),
+            quantity: quantitys,
+            side: "Bid",
+            symbol: "SOL_USDC"
+        });
+
+        buyFlag = false;
+
+        if (orderResultBid?.status === "Filled" && orderResultBid?.side === "Bid") {
+            successBuy += 1;
+            console.log(getNowFormatDate(), "Bought successfully:", `Order number: ${orderResultBid.id}`);
+            console.log(getNowFormatDate(), "Begin wait", `Next order number`);
+            totalVolume += usdc;
+            await wait(random(5, 10) * 1000);
+            console.log(getNowFormatDate(), "End wait", `Next order ...`);
+            init(client);
+        } else {
+            if (orderResultBid?.status === 'Expired') {
+                throw new Error("Buy Order Expired");
+            } else {
+                throw new Error(orderResultBid?.status);
+            }
+        }
+    } catch (error) {
+        console.log(getNowFormatDate(), `Error in buying: ${error.message}`);
+        console.log("=======================");
+    }
+};
 
 (async () => {
-    const apisecret = API_SECRET;
-    const apikey = API_KEY;
-    const client = new backpack_client_1.BackpackClient(apisecret, apikey);
+    const apiSecret = API_SECRET;
+    const apiKey = API_KEY;
+    const client = new BackpackClient(apiSecret, apiKey);
     init(client);
-})()
+})();
